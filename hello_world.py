16959ea85ef43afba3c9a0b5bf3184b6a3b35a6e
Sam Dallas <sam@codeclimate.com>
2021-01-30 00:23:22 -0500

    fix hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,14 @@
 import os
-from lib.waveshare_epd import epd7in5b_V2
+from lib.waveshare_epd import epd7in5_V2
 from PIL import Image, ImageDraw, ImageFont
+import time
 
 # from: https://medium.com/swlh/create-an-e-paper-display-for-your-raspberry-pi-with-python-2b0de7c8820c
 
+pic_dir = 'pic'
+
 try:
-  display = epd7in5b_V2.EPD()
+  display = epd7in5_V2.EPD()
   display.init(display.lut_full_update)
   display.Clear(0)
 
@@ -13,10 +16,17 @@ try:
   h = display.width
   print (w, h)
 
-  image = Image.new(mode='I', size=(w, h))
+  image = Image.new('1', (h, w), 255)
   draw = ImageDraw.Draw(image)
-  draw.text((0, 0), 'Hello World.', fill=0, align='left')
+  body = ImageFont.truetype(os.path.join(pic_dir, 'RobotoMono-Regular.ttf'), 48)
+  draw.text((0, 0), 'Hello World.', font=body, fill=0, align='left')
   display.display(display.getbuffer(image))
+
+  time.sleep(3)
+  flower = Image.open('pic/flower.jpg')
+  image.paste(flower, (0, 0))
+  display.display(display.getbuffer(image))
+
 
 except IOError as e:
   print(e)
